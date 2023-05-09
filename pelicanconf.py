2187e8f33ee9095a2c33acabe463aa0251e88020
Alan Woodward <romseygeek@apache.org>
2023-05-09 12:44:07 +0100

    Remove leading zero from release date

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ SITEURL = ''
 CURRENTYEAR = date.today().year
 
 LUCENE_LATEST_RELEASE = '9.6.0'
-LUCENE_LATEST_RELEASE_DATE = datetime(2023, 5, 09)
+LUCENE_LATEST_RELEASE_DATE = datetime(2023, 5, 9)
 LUCENE_PREVIOUS_MAJOR_RELEASE = '8.11.2'
 
 # This string will be appended to all unversioned css and js resources to prevent caching surprises on edits.
