b2369d68fad6491d8938d794cbb94bce18504f0e
ChrisHegarty <chegar999@gmail.com>
2024-01-29 15:11:43 +0000

    fix date time

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ SITEURL = ''
 CURRENTYEAR = date.today().year
 
 LUCENE_LATEST_RELEASE = '9.9.2'
-LUCENE_LATEST_RELEASE_DATE = datetime(2024, 01, 29)
+LUCENE_LATEST_RELEASE_DATE = datetime(2024, 1, 29)
 LUCENE_PREVIOUS_MAJOR_RELEASE = '8.11.2'
 
 # This string will be appended to all unversioned css and js resources to prevent caching surprises on edits.
